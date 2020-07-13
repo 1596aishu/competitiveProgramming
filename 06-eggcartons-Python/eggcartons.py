0d24479e849c3d7fc1fe0ab1f35befe2431e60e7
aishu <1596aishu@gmail.com>
2020-07-13 18:49:43 +0530

    Jul 13  18:49:42

--- 06-eggcartons-Python/eggcartons.py
+++ 06-eggcartons-Python/eggcartons.py
@@ -6,7 +6,6 @@
 
 def fun_eggcartons(eggs):
 	# your code goes here
-	while eggs>0 :
-		if eggs<12: return 1
-		if eggs%12 == 0: return eggs/12
-		return eggs/12 +1
+	a = eggs%12
+	if a == 0 : return eggs//12
+	return (eggs//12) +1
