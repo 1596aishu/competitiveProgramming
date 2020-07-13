a5bf1c64cceea3a197bca439e1f8363e0087a590
aishu <1596aishu@gmail.com>
2020-07-13 23:36:05 +0530

    Jul 13  23:36:04

--- 10-handtodice-Python/handtodice.py
+++ 10-handtodice-Python/handtodice.py
@@ -9,10 +9,15 @@ def handtodice(hand):
 	# your code goes here
 	s=""
 	num = [int(d) for d in str(hand)]
-	s+=("(")
+	s+="("
 	for x in range(len(num)):
-		s+=((num[x]))
+		s+=(str(num[x]))
 		if(x != len(num)-1):
 			s+=(",")
 	s+=(")")
-	return s
+	print(s)
+handtodice(123)
+handtodice(214)
+handtodice(422)
+handtodice(400)
+handtodice(101)
