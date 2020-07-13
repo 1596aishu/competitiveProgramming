0ab9fd2113c37f19e33a9baf3bd0dc5a707a62be
aishu <1596aishu@gmail.com>
2020-07-13 19:25:43 +0530

    Jul 13  19:25:43

--- 08-ismultiple-Python/ismultiple.py
+++ 08-ismultiple-Python/ismultiple.py
@@ -8,5 +8,7 @@
 def fun_ismultiple(m, n):
 	if m==0 and n==0: return True
 	if n!=0:	
-		if m%n==0: return True
-	return False # replace with your solution
+		if m%n==0: 
+			return True
+		else:
+			return False # replace with your solution
