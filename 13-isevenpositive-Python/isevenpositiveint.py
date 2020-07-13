3a60908e93d50e384f92d80df5a4b602e890b71d
aishu <1596aishu@gmail.com>
2020-07-14 00:20:34 +0530

    Jul 14  00:20:34

--- 13-isevenpositive-Python/isevenpositiveint.py
+++ 13-isevenpositive-Python/isevenpositiveint.py
@@ -22,15 +22,5 @@ def isevenpositiveint(x):
 		return False
 	
 
-isevenpositiveint(9)
-isevenpositiveint(-1)
-isevenpositiveint(-2)
-isevenpositiveint(-3)
-isevenpositiveint(2)
-isevenpositiveint(3)
-isevenpositiveint(1.0)
-isevenpositiveint("yikes!")
-isevenpositiveint(None)
+
 isevenpositiveint((12))
-isevenpositiveint([12])
-isevenpositiveint(123456)
