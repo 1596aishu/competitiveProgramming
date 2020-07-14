39309d59bc9ac7e067386118d65ccc7f262d9ed5
aishu <1596aishu@gmail.com>
2020-07-14 18:45:22 +0530

    Jul 14  18:45:22

--- 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
+++ 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
@@ -14,15 +14,16 @@
 #       trow=[l+r for l,r in zip(trow+y, y+trow)]
 #    return n>=1
 # pascal_triangle(6)
-def pascal(N):
-    Pascal = np.zeros((N, N), dtype=object)
-    Pascal[0, 0] = 1
-    Pascal[1,0] = Pascal[1,1] = 1
-    for n in range(2, N):
-        for r in range(0, n+1):
-            Pascal[n, r] = Pascal[n-1, r-1] + Pascal[n-1, r]
+
 
 
 def fun_pascaltrianglevalue(row, col):
-
-   return 
+   a = []
+   for i in range(n):
+      a.append([])
+      a[i].append(1)
+      for j in range(1,i):
+         a[i].append(a[i-1][j-1]+a[i-1][j])
+         if(n!=0):
+            a[i].append(1)
+   return a[row-1][col]
