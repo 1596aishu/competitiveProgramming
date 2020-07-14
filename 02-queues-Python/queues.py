5f8d9fea1dea112daaa33bb1aaf58168e3df0ed6
aishu <1596aishu@gmail.com>
2020-07-14 14:09:47 +0530

    Jul 14  14:09:47

--- 02-queues-Python/queues.py
+++ 02-queues-Python/queues.py
@@ -6,14 +6,14 @@ Make sure you pass the test cases too!"""
 
 class Queue:
     def __init__(self, head=None):
-        self.storage = [head]
+        self.storage = [head]  
 
     def enqueue(self, new_element):
         self.storage.append(new_element)
-
+        
     def peek(self):
         return self.storage[0]
-        
+
     def dequeue(self):
         a = self.storage[0]
         for i in range(len(self.storage)-1):
