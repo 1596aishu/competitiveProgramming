9ecbcd41b618264f2d1f7b5eed76f28b0b3333fd
aishu <1596aishu@gmail.com>
2020-07-14 13:04:26 +0530

    Jul 14  13:04:25

--- 01-stacks-Python/stacks.py
+++ 01-stacks-Python/stacks.py
@@ -47,9 +47,8 @@ class stack(object):
     def push(self, new_element):
         "Push (add) a new element onto the top of the stack"
         # pass
-        a = LinkedList(new_element)
         if self.ll:
-            a.next = new_element
+            ll.next = new_element
             self.top = new_element 
         else:
             top = LinkedList(new_element)
