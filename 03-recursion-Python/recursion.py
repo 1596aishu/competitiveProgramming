c13aaa2ed65158ae6d62b9aee88f589ea2e626bf
aishu <1596aishu@gmail.com>
2020-07-14 14:15:34 +0530

    Jul 14  14:15:33

--- 03-recursion-Python/recursion.py
+++ 03-recursion-Python/recursion.py
@@ -4,4 +4,4 @@ Your code should have the same input/output as the
 iterative code in the instructions."""
 
 def get_fib(position):
-    return -1
+    lambda position: position if position<=1 else get_fib(position-1) +get_fib(position-2)
