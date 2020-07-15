b7f36d718d1eff33818757b824021a86c87eef10
aishu <1596aishu@gmail.com>
2020-07-15 19:34:16 +0530

    Jul 15  19:34:14

--- 01-nth_happy_number-Python/nth_happy_number.py
+++ 01-nth_happy_number-Python/nth_happy_number.py
@@ -11,42 +11,5 @@
 # assert(nthHappyNumber(7) == 31)
 
 
-def sqares(n):
-	sum = 0
-	while(n > 0):
-		x = n % 10
-		sum += x*x
-		n = n//10
-	return sum
-
-def ishappynumber(n):
-	z = []
-	if(n > 1):
-		while(True):
-			n = sqares(n)
-			if(n == 1):
-				return True	
-			elif n in z:
-				return False
-			else:
-				z.append(n)	
-	else:
-		return False
-  
-
 def fun_nth_happy_number(n):
-	count = 0
-	i = 1
-	if n == 0:
-		return 1
-	elif n>0:
-		while(count!=n):
-			
-			while(True):
-				i += 1
-				if(ishappynumber(i)):
-					break
-
-			count += 1
-		# print(i)
-		return i
+	return 0
