663e124b96a6e29583e1d02ab50ad391ed87b1ed
aishu <1596aishu@gmail.com>
2020-07-16 14:07:08 +0530

    Jul 16  14:07:07

--- 06-replace-Python/replace.py
+++ 06-replace-Python/replace.py
@@ -3,7 +3,24 @@
 # replace(s1, s2, s3) that returns a string equal to 
 # s1.replace(s2, s3), but again without calling s.replace().
 
-
+def strings(s4,s,j):
+	a=""
+	for i in range(0,len(s4)):
+		if i !=len(s4)-1:
+			a+=s4[i]+s
+		else:
+			a+=s4[i]
+		return a
+  
 def fun_replace(s1, s2, s3):
-	return s1
-
+	s4=s1.split(s2)
+	if(len(s4[0])==len(s1)):
+		return s1
+	else:
+		s=""
+		i=0
+		if(len(s4[0])==0):
+			i=1
+			s+=s3
+		s=strings(s4,s3,i)
+	return s
