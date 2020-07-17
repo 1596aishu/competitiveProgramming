0ad92fe2288a6ac3ae833cf40d044c769e573216
aishu <1596aishu@gmail.com>
2020-07-17 12:15:19 +0530

    Jul 17  12:15:17

--- 08-nth_additive_prime-Python/nth_additive_prime.py
+++ 08-nth_additive_prime-Python/nth_additive_prime.py
@@ -17,14 +17,11 @@ def fun_nth_additive_prime(n):
 	i = 2
 	while c!=n:		
 		if isprime(i):
-			# print(i)
+			print(i)
 			sum=0
-			temp = i
-			while(temp != 0):
-				x = temp % 10
-				sum +=x
-				temp = temp // 10
-				print(sum)
+			temp = str(i)
+			for x in temp:
+				sum+=int(x)
 			if isprime(sum):
 				c+=1
 		i+=1
