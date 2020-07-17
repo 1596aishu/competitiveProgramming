0eda00553de34f882d07005e8c9daf8f70dd9216
aishu <1596aishu@gmail.com>
2020-07-17 12:38:14 +0530

    Jul 17  12:38:13

--- 10-isrotation-Python/isrotation.py
+++ 10-isrotation-Python/isrotation.py
@@ -5,6 +5,22 @@
 # 3412 is a rotation of 1234. Any number 
 # is a rotation of itself.
 
-def isrotation(x, y):
+def isrotation(str1, str2):
 	# Your code goes here
-	pass
+	# pass
+	sting=""
+	str3 = str2[:1:]
+	for x in str1:
+		if str3 != x:
+			sting+=x
+		if str3==x:
+			break
+	for y in range(len(str1)):
+		if str1[y]==str3:
+			str4=str1[y::]
+	result = str4+sting
+	print(result)
+	if result==str2:
+		return True
+	else:
+		return False
