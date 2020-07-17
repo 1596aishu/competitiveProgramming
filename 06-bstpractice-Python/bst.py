809342083c98d3be5d5c31002b80767662f046b9
aishu <1596aishu@gmail.com>
2020-07-17 20:06:49 +0530

    Jul 17  20:06:48

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -1,3 +1,5 @@
+# import sys
+# sys.setrecursionlimit(10**6) 
 class Node(object):
     def __init__(self, value):
         self.value = value
@@ -9,34 +11,46 @@ class BST(object):
     def __init__(self, root):
         self.root = Node(root)
 
-    def insert_bst(self, root, val):
-        if root is None: 
-                root.value = val 
-        else: 
-            if self.root.value < val: 
-                if self.root.right is None: 
-                    self.root.right = val 
-                else: 
-                    self.insert_bst(self.root.right, val) 
-            else: 
-                if self.root.left is None: 
-                    self.root.left = val 
-                else: 
-                    self.insert_bst(self.root.left, val)
-
-    def insert(self, new_val):
-        self.insert_bst(self.root,new_val)
+    def insert(self, d):
+        if self.root.value == d:
+            return False
+        elif d < self.root.value:
+            if self.root.left:
+                self.root = self.root.left
+                return self.insert(d)
+            else:
+                self.root.left = Node(d)
+                return True
+        else:
+            if self.root.right:
+                self.root = self.root.right
+                return self.insert(d)
+            else:
+                self.root.right = Node(d)
+                return True
 
     def printSelf(self):
         # Your code goes here
-        # pass
-        if self.root is None:
-            print("None")
-        else:
-            print(self.root.left)
-            print(self.root.right)
+        pass
+        # if self.root is None:
+        #     print("None")
+        # else:
+        #     print(self.root.left)
+        #     print(self.root.right)
         
     def search(self, find_val):
         # Your code goes here
         pass
+        # if self.root is None or self.root.val == find_val: 
+        #     return self.root 
+        # if self.root.val < find_val: 
+        #     return search(self.root.right,find_val) 
+        
+        # return search(root.left,find_val)
 
+tree = BST(4)
+tree.insert(2)
+tree.insert(1)
+tree.insert(3)
+tree.insert(5)
+# tree.printSelf()
