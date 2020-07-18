626a3d481a3cf080a15c8cad370f8323d1549dcf
aishu <1596aishu@gmail.com>
2020-07-18 13:01:46 +0530

    Jul 18  13:01:45

--- 03-kth_occurrences-Python/kth_occurrences.py
+++ 03-kth_occurrences-Python/kth_occurrences.py
@@ -2,24 +2,36 @@
 # frequent character in the string. If there are multiple characters that 
 # can account as K-th the most frequent character then, print any one of them.
 
-
-
 def fun_kth_occurrences(s, n):
 	l = []
 	l1 = []
 	c = []
 	for i in s:
-		if i not in l1:
+
+		if i not in l1 and i != " ":
 			z = s.count(i)
+			
 			if z not in c:
 				c.append(z)
 				l1.append(i)
 				if z == n-1:
 					print(i)
 				# return i
+				
 				l.append((z,i))
+		
+
 	l = sorted(l)
 	# l = l.sort(reverse = True)
 	print(l)
+	
 
-fun_kth_occurrences("hsdibdouvbeiovbfeibvdsioubidsbvviefadbcildfab", 4)
+	
+	x = len(l) - n
+	g = l[x][1]
+	print(g)
+	return g
+	# print(g)
+
+fun_kth_occurrences("helllo woorld", 2)
+fun_kth_occurrences("asuszenphonemaxm1 aemnsh", 6)
