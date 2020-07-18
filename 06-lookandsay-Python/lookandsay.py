abd0eff800d79821b420608d4c2df14d93515ca4
aishu <1596aishu@gmail.com>
2020-07-18 15:12:19 +0530

    Jul 18  15:12:19

--- 06-lookandsay-Python/lookandsay.py
+++ 06-lookandsay-Python/lookandsay.py
@@ -4,26 +4,32 @@
 # lookAndSay([]) == []
 # lookAndSay([1,1,1]) == [(3,1)]
 # lookAndSay([-1,2,7]) == [(1,-1),(1,2),(1,7)]
-# lookAndSay([3,3,8,-10,-10,-10]) == [(2,3),(1,8),(3,-10)]
+# lookAndSay([3,3,8,-10,-10,-10,3]) == [(2,3),(1,8),(3,-10)]
 # lookAndSay([3,3,8,3,3,3,3]) == [(2,3),(1,8),(4,3)]
 
 def lookandsay(a):
 	# Your code goes here
 	# pass
 	l = []
+	lst = []
 	c=0
 	if a ==[]:
 		return []
 	for x in a:
 		c = 0
 		i = a.index(x)
+		# print(x)
 		for j in range(i,len(a)):
+			print(a[j])
 			if x != a[j]:
 				break
 			c+=1
 		l.append((c,x))
-	lst = (l)
-	print(lst)
+	for i in l:
+		if i not in lst:
+			lst.append(i)
+	# print(lst)
+	
 	return(list(lst))
-lookandsay([3,3,8,-10,-10,-10])
+lookandsay([3,3,8,-10,-10,-10,3])
 				
