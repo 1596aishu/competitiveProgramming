44cb7764620d72bb00ec9d6a185f24b94e93b74f
aishu <1596aishu@gmail.com>
2020-07-18 12:10:57 +0530

    Jul 18  12:10:56

--- 03-kth_occurrences-Python/kth_occurrences.py
+++ 03-kth_occurrences-Python/kth_occurrences.py
@@ -5,6 +5,22 @@
 
 
 def fun_kth_occurrences(s, n):
-	return 'a'
+	d = {}
+	l = []
+	l1 = []
+	for i in s:
+		if i not in l1:
+			z = s.count(i)
+			l1.append(i)
+			if z == n-1:
+				print(i)
+				# return i
+			l.append((z,i))
+	l = sorted(l)
+	# l = l.sort(reverse = True)
+	print(l)
+
+	# return 'a'
 
 
+fun_kth_occurrences("helllo woorld", 2)
