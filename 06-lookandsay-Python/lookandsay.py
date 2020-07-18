d4389c43cfccc916f3100d5d85389dc0585248ec
aishu <1596aishu@gmail.com>
2020-07-18 17:10:56 +0530

    Jul 18  17:10:56

--- 06-lookandsay-Python/lookandsay.py
+++ 06-lookandsay-Python/lookandsay.py
@@ -7,28 +7,17 @@
 # lookAndSay([3,3,8,-10,-10,-10]) == [(2,3),(1,8),(3,-10)]
 # lookAndSay([3,3,8,3,3,3,3]) == [(2,3),(1,8),(4,3)]
 
+
 def lookandsay(a):
-	# Your code goes here
-	# pass
-# 	l = []
-# 	lst = []
-# 	c=0
-# 	if a ==[]:
-# 		return []
-# 	for x in a:
-# 		c = 0
-# 		i = a.index(x)
-# 		for j in range(i,len(a)):
-# 			if x != a[j]:
-# 				break
-# 			c+=1
-# 		l.append((c,x))
-# 	for i in l:
-# 		if i not in lst:
-# 			lst.append(i)
-# 	print(list(lst))
-# 	return(list(lst))
-# lookandsay([3,3,8,-10,-10,-10])
+	c=1
+	l = []
+	for i in range(1,len(a)+1):
+		if i<len(a) and a[i-1] == a[i]:
+			c += 1
+		else:
+			l.append((c,a[i-1]))
+			c = 1
+	return(l)
   
 	l = []
 	lst = []
@@ -48,10 +37,10 @@ def lookandsay(a):
 	# 	if i not in lst:
 	# 		lst.append(i)
 	for i in range(1,len(l)):
-		if i-1>1:
+		if i-1>i:
 			lst.append(i-1)
 		# result = all(x < y for x, y in zip(i, i+1))
-	# print(lst)
+	print(lst)
 	
 	return(list(lst))
 lookandsay([3,3,8,3,3,3,3])
