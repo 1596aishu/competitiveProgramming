c3e135affa38255ccaae403065ec4e00ed925a50
aishu <1596aishu@gmail.com>
2020-07-18 15:33:09 +0530

    Jul 18  15:33:09

--- 06-lookandsay-Python/lookandsay.py
+++ 06-lookandsay-Python/lookandsay.py
@@ -22,11 +22,13 @@ def lookandsay(a):
 			if a[x] != a[j]:
 				break
 			c+=1
-	l.append((c,a[x]))
+		if x<len(a)-1:
+			if a[x]!=a[x+1]:
+				l.append((c,a[x]))
 	for i in l:
 		if i not in lst:
 			lst.append(i)
-	print(l)
+	print(set(l))
 	
 	return(list(lst))
 lookandsay([3,3,8,8,-10,-10,-10,8,8,8])
