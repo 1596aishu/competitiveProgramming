7b0162aa8d335cc9a303d54b99de92eca732b8f1
aishu <1596aishu@gmail.com>
2020-07-21 16:43:13 +0530

    Jul 21  16:43:12

--- 02-carrylessadd-Python/carrylessadd.py
+++ 02-carrylessadd-Python/carrylessadd.py
@@ -7,24 +7,26 @@
 
 def fun_carrylessadd(x, y):
 	c = ""
-	while x!=0 and y!=0:
-		r = x%10
-		print(r)
-		s = y%10
-		print(s)
-		if len(str(x))>1 and len(str(y))>1:
-			if r+s>9:
-				c += str((r+s)%10)
-			x = x//10
-			y = y//10
-		else:
-			if r+s>9:
-				c+=str((r+s)%10)
+	if len(str(x))==len(str(y)):
+
+		while x!=0 and y!=0:
+			r = x%10
+			print(r)
+			s = y%10
+			print(s)
+			if len(str(x))>1 and len(str(y))>1:
+				if r+s>9:
+					c += str((r+s)%10)
+				x = x//10
+				y = y//10
 			else:
-				c = str(r+s)
-				# print(c)
-			x = 0
-			y = 0
+				if r+s>9:
+					c+=str((r+s)%10)
+				else:
+					c = str(r+s)
+					# print(c)
+				x = 0
+				y = 0
 	c = c[::-1]
 	print(int(c))
 	return int(c)
