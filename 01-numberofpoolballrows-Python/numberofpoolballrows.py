977e0144939080cb332f9dca838e77e1a431a977
aishu <1596aishu@gmail.com>
2020-07-21 14:44:13 +0530

    Jul 21  14:44:13

--- 01-numberofpoolballrows-Python/numberofpoolballrows.py
+++ 01-numberofpoolballrows-Python/numberofpoolballrows.py
@@ -4,7 +4,7 @@
 # smallest int number of balls required for the given number of pool balls. Thus, numberOfPoolBallRows(6) 
 # returns 3. Note that if any balls must be in a row, then you count that row, and so 
 # numberOfPoolBallRows(7) returns 4 (since the 4th row must have a single ball in it).
-c = 0
+
 def numberofballrows(balls):
 	n = 1
 	while n*(n-1)//2 <balls:
@@ -13,8 +13,6 @@ def numberofballrows(balls):
 			
 
 def fun_numberofpoolballrows(balls):
-	global c
-	c = 0
 	return numberofballrows(balls)
 
-fun_numberofpoolballrows(7)
+# fun_numberofpoolballrows(7)
