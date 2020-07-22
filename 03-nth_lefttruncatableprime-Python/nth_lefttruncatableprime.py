564fc4ac162427d8b11d110126ca88b7c06faf9a
aishu <1596aishu@gmail.com>
2020-07-22 19:21:28 +0530

    Jul 22  19:21:28

--- 03-nth_lefttruncatableprime-Python/nth_lefttruncatableprime.py
+++ 03-nth_lefttruncatableprime-Python/nth_lefttruncatableprime.py
@@ -3,7 +3,7 @@
 # and which remains prime when the leading (left) digit is successively removed. 
 # For example, 317 is left-truncatable prime since 317, 17 and 7 are all prime. 
 # There are total 4260 left-truncatable primes.
-# So nthLeftTruncatablePrime(0) retunearestKaprekarNumber(n)rns 2, and 
+# So nthLeftTruncatablePrime(0) retuns 2, and 
 # nthLeftTruncatablePrime(10) returns 53.
 
 
@@ -11,4 +11,5 @@
 import math
 
 def fun_nth_lefttruncatableprime(n):
+    
     return 1
