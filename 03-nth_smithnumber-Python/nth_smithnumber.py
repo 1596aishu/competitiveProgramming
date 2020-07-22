88edd6bc5ec5c82227e58578fc13c282ca6b88c0
aishu <1596aishu@gmail.com>
2020-07-22 13:08:27 +0530

    Jul 22  13:08:25

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -25,8 +25,7 @@ def smithnumber(n):
                 l.append(i)
                 n //= i
         i += 1
-    for i in l:
-        fac_sum+=i
+    sum(list(map(int,list(str(n)))))
     for j in str(p):
         digit_sum+=int(j)
     if fac_sum == digit_sum:
