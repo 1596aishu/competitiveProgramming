dc41a61af29fb3e72519123e1c17a4ffa0eb0633
aishu <1596aishu@gmail.com>
2020-07-22 18:40:12 +0530

    Jul 22  18:40:12

--- 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
+++ 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
@@ -11,28 +11,25 @@ def isprime(n):
 	return True
 
 def primeFactors(n):
-   # no of even divisibility
-   while n % 2 == 0:
-      print (2),
-      n = n / 2
-   # n reduces to become odd
-   for i in range(3,int(math.sqrt(n))+1,2):
-      # while i divides n
-      while n % i== 0:
-         print (i)
-         n = n / i
-   # if n is a prime
-   if n > 2:
-      print (n)
+	# no of even divisibility
+	while n % 2 == 0:
+		print (2),
+		n = n / 2
+	# n reduces to become odd
+	for i in range(3,int(math.sqrt(n))+1,2):
+		# while i divides n
+		while n % i== 0:
+			print (i)
+			n = n / i
+	# if n is a prime
+	if n > 2:
+		print (int(n))
+	return n
+	
 
 def powerfulNumber(n):
 	l = []
 	l1 = []
-	for i in range(2,n):
-		while n%i == 0:
-			if isprime(i):
-				l.append(i)
-			i+=1
 	for i in l:
 		if n%(i**2) == 0:
 			l1.append(i)
