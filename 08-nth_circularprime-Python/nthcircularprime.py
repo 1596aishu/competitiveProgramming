5c160aff89a9a0c11e4e1af9141ed21ab9226b50
aishu <1596aishu@gmail.com>
2020-07-22 15:31:08 +0530

    Jul 22  15:31:07

--- 08-nth_circularprime-Python/nthcircularprime.py
+++ 08-nth_circularprime-Python/nthcircularprime.py
@@ -34,8 +34,7 @@ def nthcircularprime(n):
 	i = 2	
 	while c!=n:
 		# print(c)
-		if isprime(i):
-			if "0" not in str(i):			
+		if isprime(i):		
 				if circularprime(i):
 					# print("circular prime:"+i)
 					c+=1
