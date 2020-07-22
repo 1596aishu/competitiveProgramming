eced4d4eede1f550a3cad2e4666604fc319199cb
aishu <1596aishu@gmail.com>
2020-07-22 12:40:00 +0530

    Jul 22  12:40:00

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -8,5 +8,37 @@
 # so fun_nthsmithnumber(1) should return 22
 
 
+
+def isprime(n):
+	if n > 1:
+		for i in range(2,n):
+			if n % i ==0:
+				return False
+		return True
+
+def smithnumber(n):
+    p = n
+    i = 2
+    l = []
+    while(n>1):
+        if isprime(i):
+            while(n%i == 0 and n >1):
+                l.append(i)
+                n //= i
+        i += 1
+    if sum2(p) == sum1(l):
+        # print(sum1(l), l)
+        return True
+    return False
+
 def fun_nth_smithnumber(n):
-    return 1
+    i = -1
+    j = 2
+    # return smithnumber(22)
+    while(i < n):
+        if isprime(j) != True:
+            if smithnumber(j):
+                # print(j)
+                i += 1
+        j += 1
+    return j-1
