f042f0720bef17d02e0e06f7ce381af83ac40413
aishu <1596aishu@gmail.com>
2020-07-23 18:49:59 +0530

    Jul 23  18:49:58

--- 03-nth_lefttruncatableprime-Python/test_nth_lefttruncatableprime.py
+++ 03-nth_lefttruncatableprime-Python/test_nth_lefttruncatableprime.py
@@ -7,7 +7,6 @@ import pytest
 @pytest.mark.parametrize('a, result',[
    (0,2),(1, 3),(5, 17),(10,53),
 (15,113),(20,223),(25,347),
-
 ])
 def test_fun_nth_lefttruncatableprime(a, result):
     assert fun_nth_lefttruncatableprime(a) == result
