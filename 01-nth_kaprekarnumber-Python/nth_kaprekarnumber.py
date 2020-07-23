b69a7b62bc4b8c72daf83a92674bbd3cad482b67
aishu <1596aishu@gmail.com>
2020-07-23 19:35:06 +0530

    Jul 23  19:35:05

--- 01-nth_kaprekarnumber-Python/nth_kaprekarnumber.py
+++ 01-nth_kaprekarnumber-Python/nth_kaprekarnumber.py
@@ -14,8 +14,8 @@ def nthKaprekarNumber(n):
     sq = str(sq)
     sq1 = sq[:(len(sq)//2)]
     sq2 = sq[(len(sq)//2):]
-    sq3 = int(float(sq1))
-    sq4 = int(float(sq2))
+    sq3 = int(sq1)
+    sq4 = int(sq2)
     sum1 = sq3+sq4
     print(sq1,sq2)
     # sum = 0
