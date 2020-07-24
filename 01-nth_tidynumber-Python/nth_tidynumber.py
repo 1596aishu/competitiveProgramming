e154e81ec47d2f6473cd72b6cf048ba959baae62
aishu <1596aishu@gmail.com>
2020-07-24 14:31:09 +0530

    Jul 24  14:31:09

--- 01-nth_tidynumber-Python/nth_tidynumber.py
+++ 01-nth_tidynumber-Python/nth_tidynumber.py
@@ -6,5 +6,14 @@
 # fun_nth_tidynumber(15) = 17
 # fun_nth_tidynumber(35) = 46
 
-def fun_nth_tidynumber(n):
+def nthTidyNumber(n):
     return 0
+
+def fun_nth_tidynumber(n):
+    i = 0
+    j = 2
+    while i<n:
+        if nthTidyNumber(j):
+            i+=1
+        j+=1
+    return j-1
