75e068f89b35494e4f40b34ae9a792703ad46136
aishu <1596aishu@gmail.com>
2020-07-24 14:52:43 +0530

    Jul 24  14:52:42

--- 02-nth_uglynumber-Python/nth_uglynumber.py
+++ 02-nth_uglynumber-Python/nth_uglynumber.py
@@ -10,14 +10,11 @@ def isprime(n):
 
 def primefactors(n):
     l = []
-    if n<10:
-        if isprime(n):
-            l.append(n)
-    else:
-        for i in range(2,n):
-            if n%i == 0:
-                if isprime(i):
-                    l.append(i)
+
+    for i in range(2,n):
+        if n%i == 0:
+            if isprime(i):
+                l.append(i)
     print(n,l)
     return l
 
