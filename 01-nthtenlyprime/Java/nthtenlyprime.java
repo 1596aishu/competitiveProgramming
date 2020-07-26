ed886e74172e1d50d69552e90f1c2b1e348c23e9
aishu <1596aishu@gmail.com>
2020-07-26 12:39:19 +0530

    Jul 26  12:39:19

---
+++ 01-nthtenlyprime/Java/nthtenlyprime.java
@@ -0,0 +1,28 @@
+
+class nthtenlyprime {
+	public int fun_nthtenlyprime(int n){
+		// return 1;
+		int i = 0;
+		int j = 2;
+		int sum = 0;
+		while(i<n){
+			while(i!=0) {
+				sum += i%10;
+				i = n/10;
+			}
+			if (sum == 10){
+				i+=1;
+			}
+			j+=1;
+		}
+		
+		return j-1;
+	}
+
+	public static void main(String[] args) {
+		nthtenlyprime s = new nthtenlyprime();
+		int x = s.fun_nthtenlyprime(0);
+		System.out.println(x);
+		
+	}
+}
